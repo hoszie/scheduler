fae2646aa0c7a298901808f92b5deb3de0ca7c6e
Nick Hoszko <nickhoszko@Nicks-MacBook-Pro.local>
2019-11-25 15:50:55 -0800

    Added task and daylistitem stories

--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,8 @@ import "index.scss";
 
 import Button from "components/Button";
 
+import DayListItem from "components/DayListItem";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -21,4 +23,46 @@ storiesOf("Button", module)
     <Button disabled onClick={action("button-clicked")}>
       Disabled
     </Button>
+  ));
+
+
+/* Storybook is a test environment. We need to import the component that we are testing. */
+import Task from "../taskbox/src/components/Task.js";
+
+/* This is fake data that we can pass as a prop to each version of our Task. */
+export const task = {
+  id: "1",
+  title: "Test Task",
+  state: "TASK_INBOX",
+  updatedAt: new Date(2018, 0, 1, 9, 0)
+};
+
+/* We pass these actions below using the spread operator */
+export const actions = {
+  onPinTask: action("onPinTask"),
+  onArchiveTask: action("onArchiveTask")
+};
+
+/* We start a sequence of stories, but calling the imported `storiesOf` function. */
+storiesOf("Task", module)
+  /* Our first story, called 'default', renders a basic Task with both actions. */
+  .add("default", () => <Task task={task} {...actions} />)
+  /* Our second story, called 'pinned', clones the task, and sets its state to "TASK_PINNED". */
+  .add("pinned", () => (
+    <Task task={{ ...task, state: "TASK_PINNED" }} {...actions} />
+  ))
+  /* Our third story, called 'archived', clones the task, and sets its state to "TASK_ARCHIVED". */
+  .add("archived", () => (
+    <Task task={{ ...task, state: "TASK_ARCHIVED" }} {...actions} />
+  ));
+
+  storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  }) // Provides the default background color for our component
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />) // To define our stories, we call add() once for each of our test states to generate a story
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));

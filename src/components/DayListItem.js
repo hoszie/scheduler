874964263410b34c7da5f46f7c7349866ec61637
Nick Hoszko <nickhoszko@Nicks-MacBook-Pro.local>
2019-11-29 12:03:11 -0800

    Added InterviewList files

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,16 +3,28 @@ import classNames from "classnames";
 import "./DayListItem.scss";
 
 export default function DayListItem(props) {
+
+
+  const formatSpots = function(spotsRemaining) {
+
+    if (spotsRemaining === 1) {
+      return '1 spot remaining';
+    }
+    if (spotsRemaining === 0) {
+      return 'no spots remaining';
+    } 
+    return `${spotsRemaining} spots remaining`;
+  }
   const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
   });
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
-  
 }
 

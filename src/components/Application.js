64591266d36766bd07479b39adab8dbf4e1f9784
Nick Hoszko <nickhoszko@Nicks-MacBook-Pro.local>
2019-11-29 12:01:40 -0800

    Added useEffect and the bookInterview function

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,10 +1,72 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Appointment from "components/Appointment";
 import "components/Application.scss";
+import DayList from "./DayList";
+import {getAppointmentsForDay, 
+        getInterview,
+        getInterviewersForDay } from "../helpers/selectors"
 
-export default function Application(props) {
+export default function Application() {
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    setState({
+      ...state,
+      appointments
+    });
+    axios.put(`/api/appointments/${id}`, { interview });
+
+    console.log(id, interview);
+  }
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {},
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const appointments = getAppointmentsForDay(state, state.day)
+  const interviewers = getInterviewersForDay(state, state.day);
+
+  useEffect(() => {
+    Promise.all([
+      axios(`/api/days`),
+      axios(`/api/appointments`),
+      axios(`/api/interviewers`)
+    ]).then((all) => {
+        setState(prev => ({
+          ...prev, 
+          days: all[0].data, 
+          appointments: all[1].data,
+          interviewers: all[2].data,
+        }));
+    })
+  }, []);
+
+  const appointmentList = appointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+    return (<Appointment 
+            key={appointment.id} 
+            id={appointment.id}
+            time={appointment.time}
+            interview={interview}
+            interviewers={interviewers}
+            bookInterview={bookInterview}
+            />
+          );
+  })
+
   return (
-    
     <main className="layout">
       <section className="sidebar">
         <img
@@ -13,7 +75,13 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={state.days}
+            day={state.day}
+            setDay={setDay}
+          />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
@@ -21,7 +89,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );

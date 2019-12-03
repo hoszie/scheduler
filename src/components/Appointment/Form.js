68c87b51c40471347ca7e974781eaf71628b80b7
Nick Hoszko <nickhoszko@Nicks-MacBook-Pro.local>
2019-12-03 12:30:23 -0800

    Aid testing implementation

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import InterviewerList from "components/InterviewerList";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   function reset() {
     setName("");
@@ -21,6 +22,15 @@ export default function Form(props) {
     props.onSave(name, interviewer);
   }
 
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("");
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -32,7 +42,9 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={name}
             onChange={(event) => setName(event.target.value)}
+            data-testid="student-name-input"
           />
+        <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={props.interviewers}
@@ -43,7 +55,7 @@ export default function Form(props) {
         <section className="appointment__card-right">
           <section className="appointment__actions">
             <Button danger onClick={cancel}>Cancel</Button>
-            <Button confirm onClick={save}>Save</Button>
+            <Button confirm onClick={validate}>Save</Button>
             {/* onSubmit={event => event.preventDefault()} */}
           </section>
         </section>
